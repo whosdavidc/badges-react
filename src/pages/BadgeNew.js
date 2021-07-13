6ac24734a4f39fb98d2a27faec3668cbd7f0f13f
whosdavidc <zcastrodavid@gmail.com>
2021-07-13 17:33:27 -0300

    spreading state

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -13,6 +13,7 @@ class BadgeNew extends React.Component{
     handleChange = e => {
         this.setState({
             form: {
+                ...this.state.form,
                 [e.target.name]: e.target.value,
             },
         });
